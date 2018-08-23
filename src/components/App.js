ce35e20f02e50201537c80d6d2c60edf9170b0a3
Duncan Campbell <dkgcampbell@gmail.com>
2018-08-23 18:24:44 +0800

    feat: App component placeholder

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Would You Rather... ?
+      </div>
+    )
+  }
+}
+
+export default App
