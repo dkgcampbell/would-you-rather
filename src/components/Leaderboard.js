b05e3d64026dda5c652125b12958fff9af896bfe
Duncan Campbell <dkgcampbell@gmail.com>
2018-08-26 14:08:16 +0800

    feat: Show leaderboard unordered

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,17 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import LogIn from './LogIn';
 
-const Leaderboard = ({ authedUserId, users }) => {
-  if (!authedUserId) {
-    return <LogIn />;
+class Leaderboard extends Component {
+  renderUsers = (users) => {
+    const allUsersArray = Object.keys(users).map(userId => users[userId])
+
+    return (
+      <ol>
+        {allUsersArray.map(user => (
+          <li key={user.id}>
+            <div><strong>{user.name}</strong></div>
+            <img src={user.avatarURL} width='50' height='50' alt={user.name} />
+            <div>{user.questions.length} questions asked</div>
+            <div>{Object.keys(user.answers).length} questions answered</div>
+          </li>
+        ))}
+      </ol>
+    )
   }
 
-  return (
-    <div>
-      Leaderboard
-    </div>
-  );
+  render() {
+    const { authedUserId, users } = this.props;
+    if (!authedUserId) {
+      return <LogIn />;
+    }
+
+    return (
+      <div>
+        <h2>Leaderboard</h2>
+        {this.renderUsers(users)}
+      </div>
+    );
+  }
 };
 
 const mapStateToProps = ({ authedUserId, users }) => ({
